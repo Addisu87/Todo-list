bf899d0e4eb05f37761f0e8a12419832e8a9a24a
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-05-31 16:09:57 +0300

    Build a demo of webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,33 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist'
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource'
       }
     ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development'
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/'
   }
 };
